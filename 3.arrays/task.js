8deef686e588542f0e274d3b713852f4061cbf83
Nikolay Efremov <efrem_nn@mail.ru>
2025-04-30 22:53:05 +0700

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index])
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	return users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, idx, arr) => acc + item / arr.length, 0)
 }
